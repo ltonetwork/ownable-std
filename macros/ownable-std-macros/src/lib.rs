46f7c8410c68ea2b869999f8274d61d2ac3be51e
bekauz <bekauz@protonmail.com>
2023-03-30 14:19:02 +0200

    execute msg proc macro attribute

--- macros/ownable-std-macros/src/lib.rs
+++ macros/ownable-std-macros/src/lib.rs
@@ -1,47 +1,41 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::DataEnum;
-use syn::DeriveInput;
-use syn::parse_macro_input;
-use syn::Data::Enum;
-
+use syn::{parse_macro_input, AttributeArgs, Data::Enum, DataEnum, DeriveInput};
 
 #[proc_macro_attribute]
 pub fn ownables_std_execute_msg(metadata: TokenStream, input: TokenStream) -> TokenStream {
-    // define variants to be inserted to the input enum
-    let default_execute_variants = quote! {
-        enum DefVariants {
-            Transfer { to: Addr },
+   
+    // validate no input args
+    let args = parse_macro_input!(metadata as AttributeArgs);
+    if let Some(arg) = args.first() {
+        return syn::Error::new_spanned(arg, "no args expected")
+            .to_compile_error()
+            .into();
+    }
+
+    // define the variants to be inserted and parse into DataEnum
+    let default_execute_variants: TokenStream = quote! {
+        enum ExecuteMsg { 
+            Transfer {to: Addr},
             Lock {},
         }
-    };
-    let default_execute_variants: DeriveInput = parse_macro_input!(default_execute_variants);
-    let Enum(DataEnum {
-        variants: default_variants, ..
-    }) = default_execute_variants.data;
-
-    let mut input: DeriveInput = parse_macro_input!(input);
-    // input.data is either Struct, Enum, or Union. assert enum.
-    let variants = match input.data {
-        Enum(DataEnum { variants, ..}) => variants,
-        _ => panic!("this derive macro only works on enums"),
+    }
+    .into();
+    let default_variants_input: DeriveInput = parse_macro_input!(default_execute_variants);
+    let default_variants = match default_variants_input.data {
+        Enum(DataEnum { variants, .. }) => variants,
+        _ => panic!("only enums can provide variants"),
     };
 
-    variants.extend(default_variants.into_iter());
-    
-    metadata
+    // parse the input variants
+    let mut input_variants: DeriveInput = parse_macro_input!(input);
+    let input_variants_data = match &mut input_variants.data {
+        Enum(DataEnum { variants, .. }) => variants,
+        _ => panic!("only enums can accept variants")
+    };
+
+    // insert variants from the right to the left
+    input_variants_data.extend(default_variants.into_iter());
+
+    quote! { #input_variants }.into()
 }
-
-// #[proc_macro_attribute]
-// pub fn ownables_std_query_msg(metadata: TokenStream, input: TokenStream) -> TokenStream {
-//     let default_query_variants = quote! {
-//         enum DefVariants {
-//             GetInfo {},
-//             GetMetadata {},
-//             GetWidgetState {},
-//             IsLocked {},
-//         }
-//     };
-
-//     input
-// }

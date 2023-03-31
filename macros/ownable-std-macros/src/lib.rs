f7b918e9d48d3d942583df4c34d419fff52f8e87
bekauz <bekauz@protonmail.com>
2023-03-31 13:29:53 +0200

    instantiate_msg proc macro

--- macros/ownable-std-macros/src/lib.rs
+++ macros/ownable-std-macros/src/lib.rs
@@ -1,13 +1,13 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, Data::Enum, DataEnum, DeriveInput};
+use syn::{parse_macro_input, AttributeArgs, Data::{Enum, Struct}, DataEnum, DataStruct, DeriveInput, FieldsNamed};
 
 #[proc_macro_attribute]
 pub fn ownables_std_execute_msg(metadata: TokenStream, input: TokenStream) -> TokenStream {
    
     // validate no input args
-    let args = parse_macro_input!(metadata as AttributeArgs);
-    if let Some(arg) = args.first() {
+    let meta_ast = parse_macro_input!(metadata as AttributeArgs);
+    if let Some(arg) = meta_ast.first() {
         return syn::Error::new_spanned(arg, "no args expected")
             .to_compile_error()
             .into();
@@ -22,31 +22,31 @@ pub fn ownables_std_execute_msg(metadata: TokenStream, input: TokenStream) -> To
         }
     }
     .into();
-    let default_variants_input: DeriveInput = parse_macro_input!(default_execute_variants);
-    let default_variants = match default_variants_input.data {
+    let default_ast: DeriveInput = parse_macro_input!(default_execute_variants);
+    let default_variants = match default_ast.data {
         Enum(DataEnum { variants, .. }) => variants,
         _ => panic!("only enums can provide variants"),
     };
 
     // parse the input variants
-    let mut input_variants: DeriveInput = parse_macro_input!(input);
-    let input_variants_data = match &mut input_variants.data {
+    let mut input_ast: DeriveInput = parse_macro_input!(input);
+    let input_variants_data = match &mut input_ast.data {
         Enum(DataEnum { variants, .. }) => variants,
         _ => panic!("only enums can accept variants")
     };
 
-    // insert variants from the right to the left
+    // insert variants from the default to input
     input_variants_data.extend(default_variants.into_iter());
 
-    quote! { #input_variants }.into()
+    quote! { #input_ast }.into()
 }
 
 #[proc_macro_attribute]
 pub fn ownables_std_query_msg(metadata: TokenStream, input: TokenStream) -> TokenStream {
    
     // validate no input args
-    let args = parse_macro_input!(metadata as AttributeArgs);
-    if let Some(arg) = args.first() {
+    let meta_ast = parse_macro_input!(metadata as AttributeArgs);
+    if let Some(arg) = meta_ast.first() {
         return syn::Error::new_spanned(arg, "no args expected")
             .to_compile_error()
             .into();
@@ -66,23 +66,65 @@ pub fn ownables_std_query_msg(metadata: TokenStream, input: TokenStream) -> Toke
         }
     }
     .into();
-    let default_variants_input: DeriveInput = parse_macro_input!(default_query_variants);
-    let default_variants = match default_variants_input.data {
+    let default_ast: DeriveInput = parse_macro_input!(default_query_variants);
+    let default_variants = match default_ast.data {
         Enum(DataEnum { variants, .. }) => variants,
         _ => panic!("only enums can provide variants"),
     };
 
     // parse the input variants
-    let mut input_variants: DeriveInput = parse_macro_input!(input);
-    let input_variants_data = match &mut input_variants.data {
+    let mut input_ast: DeriveInput = parse_macro_input!(input);
+    let input_variants_data = match &mut input_ast.data {
         Enum(DataEnum { variants, .. }) => variants,
         _ => panic!("only enums can accept variants")
     };
 
-    // insert variants from the right to the left
+    // insert variants from the default to input
     input_variants_data.extend(default_variants.into_iter());
 
-    quote! { #input_variants }.into()
+    quote! { #input_ast }.into()
+}
+
+#[proc_macro_attribute]
+pub fn ownables_std_instantiate_msg(metadata: TokenStream, input: TokenStream) -> TokenStream {
+    // validate no input args
+    let meta_ast = parse_macro_input!(metadata as AttributeArgs);
+    if let Some(arg) = meta_ast.first() {
+        return syn::Error::new_spanned(arg, "no args expected")
+            .to_compile_error()
+            .into();
+    }
+
+    // define the fields to be inserted and parse into DataEnum
+    let default_instantiate_fields: TokenStream = quote! {
+        struct InstantiateMsg {
+            pub ownable_id: String,
+            pub package: String,
+            pub nft: Option<NFT>,
+            pub ownable_type: Option<String>,
+            pub network_id: u8,
+        }
+    }
+    .into();
+
+    let default_ast: DeriveInput = parse_macro_input!(default_instantiate_fields);
+    let default_fields = match default_ast.data {
+        Struct(DataStruct { fields, .. }) => fields,
+        _ => panic!("only structs can accept fields"),
+    };
+
+    let mut input_ast: DeriveInput = parse_macro_input!(input);
+    let input_fields_data = match &mut input_ast.data {
+        Struct(DataStruct { fields, .. }) => fields,
+        _ => panic!("only structs can accept fields")
+    };
+
+    // push the default fields onto the input
+    if let syn::Fields::Named(FieldsNamed { named, .. }) = input_fields_data {
+        named.extend(default_fields);
+    }
+
+    quote! { #input_ast }.into()
 }
 
 

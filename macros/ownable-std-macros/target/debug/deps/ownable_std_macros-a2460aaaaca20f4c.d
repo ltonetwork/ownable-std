d3cdfa0aab22853e49fe2345347e9dd241d0e2fc
bekauz <bekauz@protonmail.com>
2023-04-03 14:41:10 +0200

    removing target

--- macros/ownable-std-macros/target/debug/deps/ownable_std_macros-a2460aaaaca20f4c.d
+++
@@ -1,5 +0,0 @@
-/home/bekauz/Documents/lto/ownable-std/macros/ownable-std-macros/target/debug/deps/ownable_std_macros-a2460aaaaca20f4c: src/lib.rs
-
-/home/bekauz/Documents/lto/ownable-std/macros/ownable-std-macros/target/debug/deps/ownable_std_macros-a2460aaaaca20f4c.d: src/lib.rs
-
-src/lib.rs:
--- macros/ownable-std-macros/target/debug/deps/ownable_std_macros-ec0407e1fd7da55d.d
+++
@@ -1,5 +0,0 @@
-/home/bekauz/Documents/lto/ownable-std/macros/ownable-std-macros/target/debug/deps/libownable_std_macros-ec0407e1fd7da55d.so: src/lib.rs
-
-/home/bekauz/Documents/lto/ownable-std/macros/ownable-std-macros/target/debug/deps/ownable_std_macros-ec0407e1fd7da55d.d: src/lib.rs
-
-src/lib.rs:
